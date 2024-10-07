6eee08127f05cd1e0874104061e15532664d4a11
Imad FIKARA <imadfikara@icloud.com>
2024-10-07 18:21:00 +0200

    Delete tp-tf-git directory

--- tp-tf-git/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "imad-s3" {
-  bucket = "imad-s3"
-
-  tags = {
-    Name        = "imad-s3"
-    Environment = "Dev"
-  }
-}
--- tp-tf-git/tp-tf-git/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_pub1.id
-  vpc_security_group_ids = [aws_security_group.imad_sg.id]
-  tags = {
-    Name = "imad-EC2"
-  }
-}
